b979e1529f0fe4063ab4362c8aa78b36d01cc9ee
samuelololol <samuelololol@gmail.com>
2012-04-04 01:54:45 +0800

    coding style & fix prefix problem

--- kukudm/kukudmdl.py
+++ kukudm/kukudmdl.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
-import os,re,sys,string,time
-import urllib,urllib2
-import tarfile,zipfile
+import os
+import re
+import sys
+import string
+import time
+import urllib
+import urllib2
+import tarfile
+import zipfile
 import tempfile
 import shutil
 from optparse import OptionParser, OptionGroup
@@ -10,20 +16,23 @@ from BeautifulSoup import BeautifulSoup as bs
 
 TMPFOLDERPFX = 'kukudm_'
 TMP_FOLDER = tempfile.mkdtemp(prefix=TMPFOLDERPFX) + '/'
-PREFIX_LST = list()
+PREFIX_LST = []
 PREFIX_LST.append('http://ascrsbdtdb.kukudm.net:82/')
 PREFIX_LST.append('http://ascrsbdfdb.kukudm.net:81/')
 PREFIX_LST.append('http://dx.kukudm.net/')
-BROWSER_UA = 'Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.10 \
-              (KHTML, like Gecko) Chrome/8.0.552.237 Safari/534.10'
-                
-
+PREFIX_LST.append('http://hzw.socomic.com/')
+BROWSER_UA = 'Mozilla/5.0 (X11; U; Linux x86_64; en-US) \
+             AppleWebKit/534.10 (KHTML, like Gecko) \
+             Chrome/8.0.552.237 Safari/534.10'
 PREFIX_WEBSITE = "http://comic.kukudm.com"
 WEBSITE = 'http://kukudm.com/'
+CLEAR_FLD = []
 
-CLEAR_FLD = list()
+
 def cleanup():
-    '''cleanup the temp folder ever used'''
+    """ 
+    cleanup the temp folder ever used
+    """ 
     global CLEAR_FLD
     TMP_PREFIX = tempfile.gettempdir() + '/'
     for i in os.listdir(TMP_PREFIX) :
@@ -35,16 +44,25 @@ def cleanup():
         TO_BE_DELETED = TMP_PREFIX + j
         shutil.rmtree(TO_BE_DELETED)
 
-
 class kkdm_img(object):
-    """download kukudm img"""
+    """
+    download kukudm img
+    """
     url   = ''
     fname = ''
     def __init__(self, url, path, page):
         self.url   = url
         subname    = url.split('/')[-1].split('.')[-1] 
-        self.fname = path + '/' + str(page).rjust(3,'0') + '.' + subname
-        if (not os.path.exists(self.fname))or(os.path.getsize(self.fname) < 500):
+        self.fname = path + '/' + \
+                     str(page).rjust(3,'0') + \
+                     '.' + \
+                     subname
+        #if (not os.path.exists(self.fname))
+        #    or(os.path.getsize(self.fname) < 500):
+        if ((os.path.exists(self.fname)) 
+            and (os.path.getsize(self.fname) < 500)):
+            pass
+        else:
             print "retrieving the comic: %s" % self.fname
             self.dl()
 
@@ -59,24 +77,25 @@ class kkdm_img(object):
                     break
             except IOError, e:
                 if e[1] != 404:
-                    print "Warning: Retrieve %s Error.\ntime.sleep(5) and again" %self.url
+                    print "Warning: Retrieve %s Error." % self.url
+                    print "time.sleep(5) and again" 
                     time.sleep(5)
-                    #img.retrieve(self.url, self.fname)
-                    continue
-                raise RuntimeError("404, prefix problem") 
+                    raise RuntimeError("404, prefix problem") 
+                else:
+                    print "Some Network Problem .."
 
 class kkdm_vol(object):
     """kukudm vol"""
     global PREFIX_LST
     global BROWSER_UA
-    img  = list()
+    img  = []
     hder = {'User-Agent': BROWSER_UA, 'Referer': WEBSITE}
+
     def __init__(self, url, path, vol):
         self.url  = url
         self.vol  = str(vol)
         self.pfx  = PREFIX_LST[0]
         self.path = path + '/' + str(vol).rjust(3,'0') 
-
         req= urllib2.Request(url, None, kkdm_vol.hder)
         while 1:
             try:
@@ -85,24 +104,18 @@ class kkdm_vol(object):
             except urllib2.URLError, e:
                 print "samuel: (error msg):",e
                 continue
-
         sp = bs(response.read())
-        #sp  = bs(urllib2.urlopen(url).read()) # non header version
-
         jre = re.compile(r""" 
                 [(document.write)]
                 (.)*
                 ('><span)
                 """,re.VERBOSE) 
-        # create vol folder
-        if not os.path.exists(self.path):
+        if not os.path.exists(self.path):   # create vol folder
             os.makedirs(self.path)
-        # set the prefix of the vol
-        page_count = 1
+        page_count = 1                      # set the prefix of the vol
         imgo = urllib.URLopener() 
-
         n_url = url
-        while True:
+        while 1:
             img_link = self.get_img_link(sp,jre)
             try:
                 image = kkdm_img(img_link,self.path, page_count)
@@ -113,11 +126,9 @@ class kkdm_vol(object):
                 continue
             self.img.append(image)
             n_url = self.get_np_link(sp)
-
             if n_url == "http://comic.kukudm.com/exit/exit.htm":
                 break
             page_count += 1                                              
-
             req= urllib2.Request(n_url, None, kkdm_vol.hder)
             while 1:
                 try:
@@ -126,16 +137,14 @@ class kkdm_vol(object):
                 except urllib2.URLError, e:
                     print "samuel: (error msg):",e
                     continue
-
-            
             sp = bs(response.read())
-            #sp = bs(urllib2.urlopen(n_url).read())
             
     def chgpfx(self):
         for i in range(len(PREFIX_LST)):
             if self.pfx == PREFIX_LST[i]:
                 break;
         return '%s' % PREFIX_LST[((i+1)%len(PREFIX_LST))]
+
     def get_img_link(self,esp,ere):
         jimg = esp.body('table')[1]('td')[0]('script')[0]
         jimgidx = ere.finditer(str(jimg))
@@ -146,6 +155,7 @@ class kkdm_vol(object):
         img_sublink = urllib.quote(img_sublink)                           
         img_link = self.pfx + img_sublink
         return '%s' % img_link
+
     def get_np_link(self,esp):
         lenofidx =  len(esp.findAll('a'))
         nextimg_sublink = esp.findAll('a')[lenofidx - 1]['href']
@@ -155,22 +165,20 @@ class kkdm_vol(object):
 class kkdm_comic(object):
     """kukudm comic index"""
     global BROWSER_UA
-    vol = list()
-    comic_list = dict() 
+    vol = []
+    comic_list = {}
     hder = {'User-Agent': BROWSER_UA, 'Referer': WEBSITE}
 
     # initial
     def __init__(self, comic='', vols=[]):
         if comic != '':
-            self.comic = comic                           # set comic name
-            self.comic_url = self.get_book_url(comic)    # set comci url
-            comic_path = TMP_FOLDER + self.comic         # set download folder
+            self.comic = comic                         # set comic name
+            self.comic_url = self.get_book_url(comic)  # set comci url
+            comic_path = TMP_FOLDER + self.comic       # set dl folder
             req= urllib2.Request(self.comic_url, None, kkdm_comic.hder)
             if len(vols) == 0:
                 response = urllib2.urlopen(req)
                 sp = bs(response.read())
-                #sp = bs(urllib2.urlopen(self.comic_url).read())            
-
                 r_table = sp.body('table')[8]('dd')             
                 nvol = str(len(r_table))
                 vols.append(nvol)
@@ -178,8 +186,9 @@ class kkdm_comic(object):
                 c_url = self.get_vol_url(self.comic_url, int(ci))
                 self.vol.append(kkdm_vol(c_url, comic_path, int(ci)))
                 self.zipit(int(ci))
-    # get url of the comic
+
     def get_book_url(self, cname):
+        """get url of the comic"""
         self.comic_list['red']         = '514'
         self.comic_list['rome']        = '975'
         self.comic_list['naruto']      = '3'
@@ -192,8 +201,9 @@ class kkdm_comic(object):
         self.comic_list['flower']      = '1172'
         self.comic_list['wrc']         = '789'
         #error handle not be done.
-        rtn = "http://comic.kukudm.com/comiclist/" + self.comic_list[cname] +\
-               "/index.htm"
+        rtn = "http://comic.kukudm.com/comiclist/" \
+            + self.comic_list[cname] \
+            + "/index.htm"
         return '%s' % rtn
 
     def get_vol_url(self, url, vol):
@@ -206,15 +216,14 @@ class kkdm_comic(object):
                 print "samuel: (error msg):",e
                 continue
         sp = bs(response.read())
-        #sp = bs(urllib2.urlopen(url).read())
 
-        r_table = sp.body('table')[8]('dd')                 # round table
+        r_table = sp.body('table')[8]('dd')               # round table
         # try all mirro site
         for mi in [1,2,3,4]:
             try:
-                c_url = r_table[vol - 1]('a')[mi]['href']   # (1,2,3,4) are available
-                opstr =  "Comic Vol(" + str(vol) + "):" + \
-                          r_table[vol - 1]('a')[0].string
+                c_url = r_table[vol - 1]('a')[mi]['href'] #(1-4) is ok
+                opstr =  "Comic Vol(" + str(vol) + "):"\
+                         + r_table[vol - 1]('a')[0].string
                 print opstr
                 break
             except:
@@ -229,16 +238,13 @@ class kkdm_comic(object):
             return
         oldpath = os.getcwd()
         newpath = TMP_FOLDER + self.comic + '/' + volname
-
         zf = zipfile.ZipFile(zfname, mode='w') 
         os.chdir(newpath)
-
         imgfiles = os.listdir('./')
         imgfiles.reverse()
         for i in imgfiles:
             zf.write(i)
         zf.close()
-
         os.chdir(oldpath)
         img = os.listdir('./')
         print "Comic ZIP File: %s saved." % zfname
@@ -249,8 +255,6 @@ class kkdm_comic(object):
         req= urllib2.Request(url, None, kkdm_comic.hder)
         response = urllib2.urlopen(req)
         sp = bs(response.read())
-        #sp = bs(urllib2.urlopen(url).read())
-
         for i in range(0,num):
             print sp.body('dd')[i]('a')[1].string  # name
         if comic != 0:
@@ -259,24 +263,24 @@ class kkdm_comic(object):
 
 def main():
     howto = '%prog [-c comic|-u][option] arg1 arg2 ...\n\
-      \n\tRange vol download:   \t %prog -c <comic> -s <start vol> -e <end vol> \
-      \n\tDivided vol download: \t %prog -c <comic> -d vol1 [vol2 [vol3..] ] \
+      \n\tRange vols download:  \t %prog -c <comic> -s <start volnum> -e <end volnum>\
+      \n\tDivided vol download: \t %prog -c <comic> -d <volnum> [<volnums>]\
       \n\tLastest vol download: \t %prog -c <comic> -l \
       \n\tShow Lastest Update:  \t %prog -u [default counts=10]'
 
     aprs = OptionParser(usage=howto, version="%prog beta2.1")
-    aprs.add_option("-c", "--comic", dest="comic",
-                    help="indicate which comic ..")
-    aprs.add_option("-s", "--start", type="int", dest="start",
-                    help="vol start from ..")
-    aprs.add_option("-e", "--end", type="int", dest="end",
-                    help="vol end from ..")
-    aprs.add_option("-d", action="store_true", dest="divided", default=False, 
-                    help="devided download ..")
-    aprs.add_option("-l", action="store_true", dest="latest",  default=False, 
-                    help="latest vol ..")
-    aprs.add_option("-u", action="store_true", dest="update",  default=False, 
-                    help="list latest 100 updates ..")
+    aprs.add_option("-c", "--comic", dest="comic", 
+            help="indicate which comic ..")
+    aprs.add_option("-s", "--start", type="int", dest="start", 
+            help="vol start from ..")
+    aprs.add_option("-e", "--end", type="int", dest="end", 
+            help="vol end from ..")
+    aprs.add_option("-d", action="store_true", dest="divided", 
+            default=False, help="devided download ..")
+    aprs.add_option("-l", action="store_true", dest="latest",  
+            default=False, help="latest vol ..")
+    aprs.add_option("-u", action="store_true", dest="update",  
+            default=False, help="list latest 100 updates ..")
     opts, args = aprs.parse_args()
     opts = opts.__dict__
 
@@ -293,7 +297,7 @@ def main():
             x = kkdm_comic(opts["comic"])
         else:
             x = kkdm_comic(opts["comic"], range(opts["start"], 
-                           opts["end"] + 1))
+                        opts["end"] + 1))
     shutil.rmtree(TMP_FOLDER)
 
 if __name__ == '__main__':
